2e2d2c5005b36b4932e98f6ebce962b472a97c3f
André R. Pasqualotto <andre.rpasqualotto@gmail.com>
2024-10-30 15:55:52 -0300

    init repo

---
+++ app/design/page.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+export default function DesignSystem() {
+  return (
+    <>
+      <div>Buttons</div>
+      <div className="flex gap-4">
+          <Button>Button Default</Button>
+          <Button variant='noShadow'>Button variant noShadow</Button>
+          <Button variant='neutral'>Button variant neutral</Button>
+          <Button variant='reverse'>Button variant reverse</Button>
+      </div>
+
+      <div>Accordion</div>
+      <Accordion className="w-full lg:w-[unset]" type="single" collapsible>
+        <AccordionItem className="lg:w-[500px] max-w-full" value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      <div>
+      <AlertDialog>
+  <AlertDialogTrigger><Button>Open</Button></AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+      </div>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,67 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			main: '#88aaee',
+  			mainAccent: '#4d80e6',
+  			overlay: 'rgba(0,0,0,0.8)',
+  			bg: '#dfe5f2',
+  			text: '#000',
+  			border: '#000',
+  			darkBg: '#272933',
+  			darkText: '#eeefe9',
+  			darkBorder: '#000',
+  			secondaryBlack: '#212121'
+  		},
+  		borderRadius: {
+  			base: '12px'
+  		},
+  		boxShadow: {
+  			light: '4px 4px 0px 0px #000',
+  			dark: '4px 4px 0px 0px #000'
+  		},
+  		translate: {
+  			boxShadowX: '4px',
+  			boxShadowY: '4px',
+  			reverseBoxShadowX: '-4px',
+  			reverseBoxShadowY: '-4px'
+  		},
+  		fontWeight: {
+  			base: '500',
+  			heading: '800'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
